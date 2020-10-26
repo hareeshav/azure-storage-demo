d338448b6e66e1aa8a3f13fea7ea97deb58e286c
hareeshav <hareesh.veduraj@gmail.com>
2020-10-26 20:56:04 +0530

    initial commit

---
+++ src/main/java/com/demo/azure/storage/azurestoragedemo/AzurestoragedemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.azure.storage.azurestoragedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurestoragedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzurestoragedemoApplication.class, args);
+    }
+
+}
